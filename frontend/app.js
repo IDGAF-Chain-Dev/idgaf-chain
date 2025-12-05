f1163397f8b095af68c6f5e37e1e77798ade6382
IDGAF-Chain-Dev <rudalsepsl@naver.com>
2025-12-05 17:41:26 +0900

    Update L2 RPC URL to Render endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ const CONTRACTS = {
   l2: {
     bridge: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
     token: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-    rpc: 'http://localhost:8546',
+    rpc: 'https://idgaf-chain-node.onrender.com',
     chainId: 10144,
     chainName: 'IDGAF Chain'
   }
